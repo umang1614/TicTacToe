0ab26ac7c802bea9b5e591387d6037959ab1575f
Umang <imumangbhatia@gmail.com>
2021-09-28 22:04:10 +0530

    commit 1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Parcel Micro App!</h1>
+      <p>This is naaaaano .</p>
+    </>
+  );
+};
+export default App;
